3f7e603a998e077527da05d901d3fabd253c5924
lysenkolipa <olimpiada_l@ukr.net>
2017-10-30 11:52:17 +0200

    mvc added
    mvc structure  added

---
+++ core/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olimpiadalysenko
+ * Date: 13.06.17
+ * Time: 21:01
+ */
+class Controller
+{
+
+}
---
+++ core/Model.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olimpiadalysenko
+ * Date: 13.06.17
+ * Time: 21:02
+ */
+class Model
+{
+
+}
